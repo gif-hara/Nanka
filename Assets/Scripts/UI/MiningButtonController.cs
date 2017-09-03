5e7e957f684cdfa40a30fd0b78ede8c53260137f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 15:54:51 +0900

    :new: MiningButtonController追加

---
+++ Assets/Scripts/UI/MiningButtonController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HK
+{
+    [RequireComponent(typeof(Button))]
+    public sealed class MiningButtonController : MonoBehaviour
+    {
+        
+    }
+}
