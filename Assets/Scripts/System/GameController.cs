634089a32ce566d10468ff2d1a200727b7a67287
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 16:10:35 +0900

    :new: ItemSpecs持たせる

--- Assets/Scripts/System/GameController.cs
+++ Assets/Scripts/System/GameController.cs
@@ -5,9 +5,14 @@ namespace HK.Nanka
 {
     public sealed class GameController : MonoBehaviour
     {
+        [SerializeField]
+        private ItemSpecs itemSpecs;
+
         public static GameController Instance { private set; get; }
 
         public Player Player { private set; get; }
+
+        public ItemSpecs ItemSpecs { get { return this.itemSpecs; } }
 
         void Awake()
         {
