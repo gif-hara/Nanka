eef013fcc1ced64dcffe9069ffd85bf2932759a7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 14:44:20 +0900

    :new: ItemSpec追加

---
+++ Assets/Scripts/ItemSpec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace HK.Nanka
+{
+    [Serializable]
+    public sealed class ItemSpec
+    {
+        [SerializeField]
+        private string name;
+
+        public string Name { get { return this.name; } }
+    }
+}
---
+++ Assets/Scripts/ItemSpecs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace HK.Nanka
+{
+    [CreateAssetMenu(menuName = "MasterData/ItemSpecs")]
+    public sealed class ItemSpecs : ScriptableObject
+    {
+        [SerializeField]
+        private List<ItemSpec> specs;
+        
+        public List<ItemSpec> Specs { get { return this.specs; } }
+    }
+}
