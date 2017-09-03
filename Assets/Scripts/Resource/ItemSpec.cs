a5d1e621605e5983dfe664cfee2540e1fad74fe8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 16:10:13 +0900

    :new: ItemTypeとId持つように

--- Assets/Scripts/Resource/ItemSpec.cs
+++ Assets/Scripts/Resource/ItemSpec.cs
@@ -7,11 +7,21 @@ namespace HK.Nanka
     public sealed class ItemSpec
     {
         [SerializeField]
+        private int id;
+
+        [SerializeField]
         private string name;
+
+        [SerializeField]
+        private ItemType type;
 
         [SerializeField]
         private Recipe recipe;
 
+        public int Id { get { return this.id; } }
+
         public string Name { get { return this.name; } }
+
+        public ItemType Type { get { return this.type; } }
     }
 }
---
+++ Assets/Scripts/Resource/ItemType.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace HK.Nanka
+{
+    public enum ItemType
+    {
+        /// <summary>
+        /// 鉱石
+        /// </summary>
+        Ore,
+        /// <summary>
+        /// 木材
+        /// </summary>
+        Wood,
+        /// <summary>
+        /// ツール
+        /// </summary>
+        Tool,
+    }
+}
