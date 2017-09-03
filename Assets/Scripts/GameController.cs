2a07fc71aff61685e2fb36fc9a66fb8bd1530895
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 15:51:36 +0900

    :new: GameController周り追加

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Nanka
+{
+    public sealed class GameController : MonoBehaviour
+    {
+        public static GameController Instance { private set; get; }
+
+        public Player Player { private set; get; }
+
+        void Awake()
+        {
+            Assert.IsNull(Instance);
+            Instance = this;
+
+            this.Player = new Player();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HK.Nanka
+{
+    /// <summary>
+    /// プレイヤーを制御するクラス
+    /// </summary>
+    public sealed class Player
+    {
+        public Inventory Inventory { private set; get; }
+
+        public Player()
+        {
+            this.Inventory = new Inventory();
+        }
+    }
+}
