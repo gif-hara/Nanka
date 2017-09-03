94dfc6cbd1a9f48b9ff61e3d6cec776129f713e6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 21:08:19 +0900

    :new: 作成したらリストを更新するように

---
+++ Assets/Scripts/Events/Crafted.cs
@@ -0,0 +1,10 @@
+﻿using HK.Framework.EventSystems;
+using UnityEngine;
+
+namespace HK.Nanka.Events
+{
+    public sealed class Crafted : UniRxEvent<Crafted, int>
+    {
+        public int CreatedItemId { get { return this.param1; } }
+    }
+}
--- Assets/Scripts/UI/CraftElementUIController.cs
+++ Assets/Scripts/UI/CraftElementUIController.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 using UniRx;
 using HK.Framework.Text;
 using System.Text;
+using HK.Framework.EventSystems;
+using HK.Nanka.Events;
 
 namespace HK.Nanka
 {
@@ -31,20 +33,15 @@ namespace HK.Nanka
         public void Initialize(Inventory inventory, ItemSpecs specs, int itemId)
         {
             var spec = specs.Get(itemId);
-            this.nameText.text = spec.Name;
-            this.descriptionText.text = spec.Description;
-            var requireItemTextBuilder = new StringBuilder();
-            for(var i=0; i<spec.Recipe.RequireItems.Count; ++i)
-            {
-                var r = spec.Recipe.RequireItems[i];
-                requireItemTextBuilder.Append(this.requireItemFormat.Format(specs.Get(r.ItemId).Name, inventory.GetNumber(r.ItemId), r.Number));
-                if((i + 1) < spec.Recipe.RequireItems.Count)
-                {
-                    requireItemTextBuilder.AppendLine();
-                }
-            }
-            this.requireItemText.text = requireItemTextBuilder.ToString();
+            this.UpdateText(inventory, spec);
 
+            UniRxEvent.GlobalBroker.Receive<Crafted>()
+                .SubscribeWithState3(this, inventory, spec, (c, _this, _inventory, _spec) =>
+                {
+                    _this.UpdateText(_inventory, _spec);
+                })
+                .AddTo(this);
+            
             this.cachedButton.OnClickAsObservable()
                 .TakeUntilDisable(this)
                 .SubscribeWithState3(inventory, specs, spec, (_, _inventory, _specs, _spec) =>
@@ -52,8 +49,27 @@ namespace HK.Nanka
                     if(_spec.Recipe.CanCreate(_inventory))
                     {
                         Craft.Crafting(_inventory, _specs, _spec.Id);
+                        UniRxEvent.GlobalBroker.Publish(Crafted.Get(_spec.Id));
                     }
-                });
+                })
+                .AddTo(this);
+        }
+
+        private void UpdateText(Inventory inventory, ItemSpec spec)
+        {
+            this.nameText.text = spec.Name;
+            this.descriptionText.text = spec.Description;
+            var requireItemTextBuilder = new StringBuilder();
+            for(var i=0; i<spec.Recipe.RequireItems.Count; ++i)
+            {
+                var r = spec.Recipe.RequireItems[i];
+                requireItemTextBuilder.Append(this.requireItemFormat.Format(spec.Name, inventory.GetNumber(r.ItemId), r.Number));
+                if((i + 1) < spec.Recipe.RequireItems.Count)
+                {
+                    requireItemTextBuilder.AppendLine();
+                }
+            }
+            this.requireItemText.text = requireItemTextBuilder.ToString();
         }
     }
 }
