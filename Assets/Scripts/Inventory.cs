9094e9589b7a27b2bc2b0306d9eaff9514fed1df
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 14:16:08 +0900

    :new: Inventory追加

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace HK.Nanka
+{
+    public sealed class Inventory
+    {
+        public Dictionary<int, List<Item>> Items { private set; get; }
+
+        public Inventory()
+        {
+            this.Items = new Dictionary<int, List<Item>>();
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HK.Nanka
+{
+    public sealed class Item
+    {
+        public int Id { private set; get; }
+
+        public Item(int id)
+        {
+            this.Id = id;
+        }
+    }
+}
