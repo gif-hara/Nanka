11311e7be40ffa1a2a72fe1b04da289b5566ecaa
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-03 18:06:22 +0900

    :new: UIを切り替えられるように

---
+++ Assets/Scripts/UI/ChangeUIButtonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace HK.Nanka
+{
+    [RequireComponent(typeof(Button))]
+    public sealed class ChangeUIButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameUIType type;
+
+        void Awake()
+        {
+            var button = this.GetComponent<Button>();
+            button.OnClickAsObservable()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    RootUIManager.Instance.Change(_this.type);
+                });
+        }
+    }
+}
