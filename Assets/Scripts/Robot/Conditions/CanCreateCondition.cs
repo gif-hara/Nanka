7f1b33c827f2983325c15de0dc3e881abd549c0d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-09-07 02:03:34 +0900

    :new: アイテムを生成できるか

---
+++ Assets/Scripts/Robot/Conditions/CanCreateCondition.cs
@@ -0,0 +1,25 @@
+﻿using HK.Framework.Text;
+using UnityEngine;
+
+namespace HK.Nanka.RobotSystems.Conditions
+{
+    /// <summary>
+    /// アイテムを生成できるか判断するクラス
+    /// </summary>
+    [CreateAssetMenu(menuName = "MasterData/Robot/Condition/CanCreate")]
+    public sealed class CanCreateCondition : Condition
+    {
+        [SerializeField]
+        private StringAsset.Finder itemName;
+
+        public override bool Can
+        {
+            get
+            {
+                var gameController = GameController.Instance;
+                var itemSpec = gameController.ItemSpecs.Get(this.itemName.GetHashCode());
+                return itemSpec.CanCreate(gameController.Player.Inventory);
+            }
+        }
+    }
+}
